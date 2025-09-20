b75612607f7e91751220886b432f5ab40a618047
SivakumarSheshanth <harisnasivakumar111@gmail.com>
2025-09-20 16:09:38 +0530

    update hello world text

---
+++ main.py
@@ -0,0 +1,38 @@
+# This is my first python project
+print("yummy pizza")
+#variables = To a container for a values(String, Interger, Float, Boolean)
+#            A variable behaves as if it was the value contains
+#strings
+first_shop="123"
+food="pizza"
+girl="BMW"
+print(first_shop)
+print(f"Hello  {first_shop}")
+print(f"i like {food}")
+print(f"i love {girl}")
+
+#interger
+food= "pizza"
+print(f"i eat {food} like a gentleman")
+
+#float
+price=10.99
+gpa = 3
+distance= 10.22
+print(f"the bread is {price}")
+print(f"my gpa is {gpa}")
+print(f"hospital is {distance}km away")
+
+#boolean
+is_student = False
+for_sale = True
+
+if is_student :
+    print("you r a student")
+else:
+    print("you r not a student")
+
+if for_sale:
+    print("the car is for sale")
+else:
+    print("the car is not for sale")
