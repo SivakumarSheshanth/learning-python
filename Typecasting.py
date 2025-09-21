8f41fa33df50dbaf7aff988a99c5b20506ab66fc
Sivakumar Sheshanth <harisnasivakumar111@gmail.com>
2025-09-21 20:39:00 +0530

    Add files via upload

---
+++ Typecasting.py
@@ -0,0 +1,28 @@
+#Typecasting = the process of converting a variable type from one data type to another
+#              str(),  int().   float(),   bool()
+
+Name = "age"
+age=12
+gpa=3.2
+is_student = True
+
+print(type(Name))
+print(type(age))
+print(type(gpa))
+print(type(is_student))
+
+hi = str(12)
+print(hi)
+
+hi = int(12)
+print(hi)
+
+hi = float(12)
+print(hi)
+
+hi = bool(12)
+print(hi)
+
+#bcz it doesn't hv any input
+hi = bool()
+print(hi)
