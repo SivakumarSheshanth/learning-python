0f8223a6fac09123e55120b3f5d945e4c45f721f
Sivakumar Sheshanth <harisnasivakumar111@gmail.com>
2025-09-23 20:12:57 +0530

    Add files via upload

---
+++ Madlibs game.py
@@ -0,0 +1,13 @@
+# Madlibs Game
+# word game where u create a story
+# by filling in blanks with random words
+
+adjective1 = input("enter a adjective : ")
+noun= input(" enter a noun (place, person,thing): ")
+adjective2 = input("enter a adjective :")
+verb= input("enter a verb ending with 'ing'")
+adjective3 = input("enter a adjective :")
+
+print(f"i went to a {adjective1} hotel")
+print(f"there was a {noun} and that is so {adjective2}" )
+print(f"i order my dinner and start {verb} my {adjective3} food")
