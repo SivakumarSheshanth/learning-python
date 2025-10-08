13dc9b428fae8753e77600947b9195c8595d7c4d
Sivakumar Sheshanth <harisnasivakumar111@gmail.com>
2025-10-08 23:14:21 +0530

    Update conditional expression.py

--- conditional expression.py
+++ conditional expression.py
@@ -1,7 +1,6 @@
 #conditional expressions = A one line shortcut for if-else statement(ternary operator)
 #                          print or assign one or two values based on condition
 #     formula              X if condition else Y
-from errno import ECHILD
 
 num=0
 a=5
@@ -20,4 +19,5 @@ user= "admin"
 #status= "adult" if age>=18 else "child"
 #print(status)
 access_level= "full access" if user=="admin" else " not allowed"
+
 print(access_level)
