bc2e132003178a20c25a60ef8053e2299185c324
Sivakumar Sheshanth <harisnasivakumar111@gmail.com>
2025-10-11 18:22:54 +0530

    Add files via upload

---
+++ format specifiers.py
@@ -0,0 +1,38 @@
+#format specifiers={value:flags} format a value based on what flags is inserted
+
+price1= 5345.5354
+price2= 453.54
+price3= -4353.453453
+
+#print(f'the price is ${price1:.2f}')
+#print(f'the price is ${price2:.3f}')         decimals after the num
+#print(f'the price is ${price3:.1f}')
+
+#print(f'the price is ${price1:010}')
+#print(f'the price is ${price2:010}')          space for answer
+#print(f'the price is ${price3:10}')
+
+#print(f'the price is ${price1:<10}')
+#print(f'the price is ${price2:<10}')            left justify
+#print(f'the price is ${price3:<15}')
+
+#print(f'the price is ${price1:>50}')
+#print(f'the price is ${price2:>10}')            right justify
+#print(f'the price is ${price3:>15}')
+
+#print(f'the price is ${price1:^10}')
+#print(f'the price is ${price2:^10}')            center
+#print(f'the price is ${price3:^15}')
+
+#print(f'the price is ${price1:+}')
+#print(f'the price is ${price2:+}')            to put the positive sign infront
+#print(f'the price is ${price3:+}')
+
+#print(f'the price is ${price1: }')
+#print(f'the price is ${price2: }')            space in front
+#print(f'the price is ${price3: }')
+
+#print(f'the price is ${price1:,}')
+#print(f'the price is ${price2:,}')             add coma for every thousands
+#print(f'the price is ${price3:+,.2f}')
+
