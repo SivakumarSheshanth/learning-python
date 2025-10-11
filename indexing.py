2560bfb265bad97369b6d50e795ac17a0e005d1c
Sivakumar Sheshanth <harisnasivakumar111@gmail.com>
2025-10-11 17:30:06 +0530

    Add files via upload

---
+++ indexing.py
@@ -0,0 +1,16 @@
+#indexing= accessing elements of a sequence using[](indexing operator)
+#                  [start : end : step]
+
+credit_number= "134255436566"
+#print(credit_number[0])
+#print(credit_number[0:5])
+#print(credit_number[5:9])
+#print(credit_number[5:])
+#print(credit_number[-2])
+#print(credit_number[::3])
+
+last_digit = credit_number[-4:]
+print(f"these r the last digits {last_digit}")
+
+credit_number= credit_number[::-1]
+print(credit_number)
